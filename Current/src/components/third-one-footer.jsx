e19d3359bbda061ab53eed6931783326c532f4da
Villian <yuyang.he@gatech.edu>
2017-04-20 18:07:00 -0400

    Summary: Add links as required. —— Finished: —— Todo:

--- Current/src/components/third-one-footer.jsx
+++ Current/src/components/third-one-footer.jsx
@@ -19,33 +19,90 @@ import ImageWebCloudy from 'material-ui/svg-icons/image/wb-cloudy';
 
 export default class Third_One_Footer extends Component {
 
-render() {
-  var bgColors = { "Default": "#81b71a",
-                    "Blue": "#00B1E1",
-                    "Cyan": "#37BC9B",
-                    "Green": "#8CC152",
-                    "Red": "#E9573F",
-                    "Yellow": "#F6BB42",
-   };
+    render() {
+        var bgColors = {
+            "Default": "#81b71a",
+            "Blue": "#00B1E1",
+            "Cyan": "#37BC9B",
+            "Green": "#8CC152",
+            "Red": "#E9573F",
+            "Yellow": "#F6BB42",
+        };
 
-  const {selectedIndex}=this.props;
-  const {onSelect}=this.props;
-    return (
-      <Paper zDepth={1}>
-        <button type="button" onClick={() => onSelect(17)}  style={{ color: 'white',margin: '5px',float:'right', backgroundColor: '#00CCCC', border: 'none', borderRadius: '6', fontSize: '11'}}>Community Voice Mail</button>
+        const {selectedIndex} = this.props;
+        const {onSelect} = this.props;
+        return (
+            <Paper zDepth={1}>
+                <button type="button" onClick={() => onSelect(17)} style={{
+                    color: 'white',
+                    margin: '5px',
+                    float: 'right',
+                    backgroundColor: '#00CCCC',
+                    border: 'none',
+                    borderRadius: '6',
+                    fontSize: '11'
+                }}>Community Voice Mail
+                </button>
 
-        <button type="button" onClick={() => onSelect(18)}  style={{ color: 'white',margin: '5px',float:'right', backgroundColor: '#00CCCC', border: 'none', borderRadius: '6', fontSize: '11' }}>Birth Certificate</button>
+                <button type="button" onClick={() => onSelect(18)} style={{
+                    color: 'white',
+                    margin: '5px',
+                    float: 'right',
+                    backgroundColor: '#00CCCC',
+                    border: 'none',
+                    borderRadius: '6',
+                    fontSize: '11'
+                }}>Birth Certificate
+                </button>
 
-        <button type="button" onClick={() => onSelect(19)}  style={{ color: 'white',margin: '5px',float:'right', backgroundColor: '#00CCCC', border: 'none' ,borderRadius: '6', fontSize: '11'}}>Representative Payee Service</button>
+                <button type="button" onClick={() => onSelect(19)} style={{
+                    color: 'white',
+                    margin: '5px',
+                    float: 'right',
+                    backgroundColor: '#00CCCC',
+                    border: 'none',
+                    borderRadius: '6',
+                    fontSize: '11'
+                }}>Representative Payee Service
+                </button>
 
-        <button type="button" onClick={() => onSelect(20)}  style={{ color: 'white',margin: '5px',float:'right', backgroundColor: '#00CCCC', border: 'none' ,borderRadius: '6', fontSize: '11'}}> How To Get Georgia ID</button>
+                <button type="button"
+                        onClick={() => window.open('http://www.dmv.org/ga-georgia/id-cards.php')} style={{
+                    color: 'white',
+                    margin: '5px',
+                    float: 'right',
+                    backgroundColor: '#00CCCC',
+                    border: 'none',
+                    borderRadius: '6',
+                    fontSize: '11'
+                }}> How To Get Georgia ID
+                </button>
 
-        <button type="button" onClick={() => onSelect(21)}  style={{ color: 'white',margin: '5px',float:'right', backgroundColor: '#00CCCC', border:'none', borderRadius: '6', fontSize: '11'}}> Voter Registration</button>
+                <button type="button"
+                        onClick={() => window.open('http://www.dmv.org/ga-georgia/voter-registration.php')} style={{
+                    color: 'white',
+                    margin: '5px',
+                    float: 'right',
+                    backgroundColor: '#00CCCC',
+                    border: 'none',
+                    borderRadius: '6',
+                    fontSize: '11'
+                }}> Voter Registration
+                </button>
 
-       <button type="button" onClick={() => onSelect(22)}  style={{ color: 'white',margin: '5px',float:'right', backgroundColor: '#00CCCC', border: 'none', borderRadius: '6', fontSize: '11'}}>More</button>
-        
-       
-      </Paper>
-);
-  }
+                <button type="button" onClick={() => window.open('http://211online.unitedwayatlanta.org/')} style={{
+                    color: 'white',
+                    margin: '5px',
+                    float: 'right',
+                    backgroundColor: '#00CCCC',
+                    border: 'none',
+                    borderRadius: '6',
+                    fontSize: '11'
+                }}>More
+                </button>
+
+
+            </Paper>
+        );
+    }
 }
